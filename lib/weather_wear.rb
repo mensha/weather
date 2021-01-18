a4ceb79de265184745722c161d08e20283d3c406
mensha <msksalston@gmail.com>
2021-01-18 15:55:37 -0500

    new weather app

---
+++ lib/weather_wear.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+require 'pry'
+require_relative "weather_wear/version"
+require 'net/http'
+ require 'open-uri'
+ require 'json'
+ require 'pry'
+
+module WeatherWear
+  # class Error < StandardError; end
+  # Your code goes here...
+
+    class GetRequest
+
+      URL = http://api.weatherapi.com/v1/current.json
+
+        def initialize(url)
+            @url = url
+        end
+
+      
+        def get_response_body
+          uri = URI.parse(@url)
+          response = Net::HTTP.get_response(uri)
+          response.body   
+        end
+
+        
+        def parse_json
+          JSON.parse(get_response_body) #converts json returned from get_response_body into data structures.
+        end
+
+
+    class User
+
+          @@all = []
+
+          def initialize 
+            @name = name
+            @@all << self
+          end
+
+
+
+end
